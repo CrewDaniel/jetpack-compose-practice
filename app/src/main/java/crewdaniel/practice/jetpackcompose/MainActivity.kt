5f8b143a890867d8dfb18cad033a9237e8636e56
CrewDaniel <alaskay@naver.com>
2022-01-27 02:05:39 +0900

    feat: add memo list with card

--- app/src/main/java/crewdaniel/practice/jetpackcompose/MainActivity.kt
+++ app/src/main/java/crewdaniel/practice/jetpackcompose/MainActivity.kt
@@ -3,12 +3,22 @@ package crewdaniel.practice.jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons.Filled
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import crewdaniel.practice.jetpackcompose.ui.theme.JetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -16,27 +26,82 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                MyApp()
             }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String) {
-    Column {
-        Text(text = "Hello $name!")
-        Text(text = "hello again!")
+private fun MyApp() {
+    MemoList(listOf(Memo("title1", "content1"), Memo("title2", "content2")))
+}
+
+@Composable
+private fun MemoList(memos: List<Memo>) {
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+        items(items = memos) { memo ->
+            MemoCard(memo)
+        }
     }
 }
 
-@Preview(showBackground = true)
+@Composable
+private fun MemoCard(memo: Memo) {
+    Card(
+        backgroundColor = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        CardContent(memo)
+    }
+}
+
+@Composable
+private fun CardContent(memo: Memo) {
+    var expanded by remember { mutableStateOf(false) }
+
+    Row(
+        modifier = Modifier
+            .padding(12.dp)
+            .animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(12.dp)
+        ) {
+            Text(
+                text = memo.title,
+                style = MaterialTheme.typography.h4.copy(fontWeight = FontWeight.ExtraBold)
+            )
+            if (expanded) {
+                Text(text = memo.content)
+            }
+        }
+        IconButton(onClick = { expanded = !expanded }) {
+            Icon(
+                imageVector = if (expanded) Filled.ExpandLess else Filled.ExpandMore,
+                contentDescription = if (expanded) {
+                    stringResource(R.string.hide_content)
+                } else {
+                    stringResource(R.string.show_content)
+                }
+
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     JetpackComposeTheme {
-        Greeting("Android")
+        MemoList(listOf(Memo("title1", "content1"), Memo("title2", "content2")))
     }
 }
---
+++ app/src/main/java/crewdaniel/practice/jetpackcompose/Memo.kt
@@ -0,0 +1,6 @@
+package crewdaniel.practice.jetpackcompose
+
+data class Memo(
+    val title: String,
+    val content: String
+)
