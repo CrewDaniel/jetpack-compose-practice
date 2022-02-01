fb4765124f23859957ead45702ca2d84673ea347
CrewDaniel <alaskay@naver.com>
2022-02-02 00:02:35 +0900

    feat: add entity

--- app/src/main/java/crewdaniel/practice/jetpackcompose/model/Image.kt
+++ app/src/main/java/crewdaniel/practice/jetpackcompose/model/Image.kt
@@ -1,5 +1,10 @@
 package crewdaniel.practice.jetpackcompose.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "image_table")
 data class Image(
+    @PrimaryKey(autoGenerate = true) val id: Int,
     val title: String
 )
